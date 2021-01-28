50c22737d1b159e0d8a00d4d8d8dc69bdf637b80
binc0 <42704408+binc0@users.noreply.github.com>
2021-01-28 15:51:54 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -17,13 +17,8 @@ var map = L.map('map', {
     dragging: false
 });
     
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiYmluY28iLCJhIjoiY2o1NTN5dTNiMDJtdzMydGdybHh4cmFhMyJ9.SDvCSaNQC6xpSTqsJN1mCQ'
+L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}@2x.png?lang=it', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
 }).addTo(map);
 
     var fvga = {
